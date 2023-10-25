5304037c6eb6dc14ce288b15053f9eddc7a34df8
VoidGravity <abdellahbardichwork@gmal.com>
2023-10-25 11:28:01 +0100

    user story done

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
 var sideBar = document.getElementsByClassName('sideBar')[0];
+var ProductHer = document.getElementsByClassName ('ProductHer')[0];
 var CartbuttonPressed = document.getElementById('button-pressed');
 var body = document.getElementsByTagName('body')[0];
 
@@ -61,12 +62,17 @@ var orderpage_productCount = document.getElementsByClassName ('medtextsize')[0];
 var totalOrderpage = document.getElementsByClassName('shiftedtex')[4];
 var shippingFees = document.getElementsByClassName('FhipingFees')[0];
 // totalOrderpage.textContent = (totalValue.textContent * orderpage_productCount.textContent)+parseInt(shippingFees.textContent);
+if (subtotal) {
+    subtotal.textContent = totalValue.textContent;
 
-subtotal.textContent = totalValue.textContent;
+}
 function Add(){
     
     counter++;
-    orderpage_productCount.textContent = counter;
+    if (orderpage_productCount) {
+        orderpage_productCount.textContent = counter;
+
+    }
 
     totalValue.textContent = result+((counter-1)*parseInt(sideBarPrice[0].textContent));
     totalOrderpage.textContent = totalValue.textContent;
@@ -77,8 +83,10 @@ function Add(){
 function Minus(){
 
     counter--;
-    orderpage_productCount.textContent = counter;
-    console.log (orderpage_productCount.textContent);
+    if (orderpage_productCount){
+        orderpage_productCount.textContent = counter;
+
+    }
     totalValue.textContent = result+((counter-1)*parseInt(sideBarPrice[0].textContent));
 
     if (counter<=0) {
@@ -123,5 +131,59 @@ if (burgger_clicked){
     burgger_clicked.addEventListener('click',colapse);
 }
 
+var addProductButtonClicked = document.getElementsByClassName("addProduct_to_SideBar")[0];
+if (addProductButtonClicked) {
+    addProductButtonClicked.addEventListener('click',addProduct);
+
+}
+var SideBarDetailsmainstuf = document.getElementsByClassName("SideBar-detailsmainstuf")[1];
+
+function addProduct() {
+
+    var productMainDiv = document.createElement("div");
+    productMainDiv.className = "productMainDiv";
+    var img = document.createElement("img");
+    img.src = "KeyboardProduct.png";
+    img.className = "ProductNodots_js";
+    productMainDiv.appendChild(img);
+
+    var productMidDiv = document.createElement("div");
+    productMidDiv.className = "productMidDiv";
+    productMidDiv.innerHTML = '<p class="selectaVariant">Product Name</p> <p class="SideBar-Price">Product desc</p> <div class="plus1minus">                            <button class="MinusButton">                                <img src="minus.png" alt="plus sign" width="26px" height="26px">                            </button>                                                        <p class="ProductCount">1</p>                            <button class="PlusButton">                                <img src="plus.png" alt="minus sign" width="26px" height="26px">                            </button>                                                    </div>';
+
+    productMainDiv.appendChild(productMidDiv);
+    ProductHer.insertBefore(productMainDiv,SideBarDetailsmainstuf.nextSibling);
+    // ProductHer.appendChild(productMainDiv);
+}
+
+var filter = document.getElementsByClassName('filter')[0];
+var accesoriesGRP = document.getElementsByClassName('accesoriesgrp')[0];
+function showaccesories (){
+    if (accesoriesGRP.style.display=='none'){
+        accesoriesGRP.style.display='flex';
+    }
+    else {accesoriesGRP.style.display='none';}
+    
+}
+filter?.addEventListener('click',showaccesories);
+
+
+///filter list
+
+var filterlist = document.getElementsByClassName("FilterList")[0];
+var Producttags =  document.getElementsByClassName("Producttags")[0];
+
+function HideProducttags() {
+    console.log("hey");
+    if (Producttags.style.display=='flex'){
+        console.log("heeeeey1");
+        Producttags.style.display='none';
+    }
+    else{
+        console.log("heeeeey");
+        Producttags.style.display='flex';
+    }
+}
+filterlist?.addEventListener('click',HideProducttags);
 
 
